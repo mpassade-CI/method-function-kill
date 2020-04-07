76cdfa89050b98deda991ffb7cab94fa5b38e4ce
Michael Passade <michael.passade@codeimmersives.com>
2020-04-07 16:23:15 -0400

    done

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 /******************
  * YOUR CODE HERE *
  ******************/
-
+const newPerson = function(firstName='Anonymous', lastName='Person',
+age, married=false){
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+    married: married,
+    goingOn: function(){
+      return this.age + 1
+    },
+    ageUp: function(){
+      this.age++
+    },
+    getFullName: function(){
+      return `${this.firstName} ${this.lastName}`
+    },
+    marry: function(person){
+      this.married = true
+      person.married = true
+      this.spouseName = person.getFullName()
+      person.spouseName = this.getFullName()
+    },
+    divorce: function(person){
+      this.married = false
+      person.married = false
+      delete this.spouseName
+      delete person.spouseName
+    }
+  }
+}
 
 
 /*********************************
